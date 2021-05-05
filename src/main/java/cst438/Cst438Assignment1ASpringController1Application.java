fb39cc4b64142ea482465afbfe38393849386335
ndorazio <ndorazio@csumb.edu>
2021-05-04 23:03:19 -0700

    Initial Commit

---
+++ src/main/java/cst438/Cst438Assignment1ASpringController1Application.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438Assignment1ASpringController1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438Assignment1ASpringController1Application.class, args);
+	}
+
+}
